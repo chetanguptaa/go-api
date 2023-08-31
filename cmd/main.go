ab1df884af18e7127057f0656647bc043338fdb1
chetan gupta <chetangupta123raj@gmail.com>
2023-08-31 12:55:13 +0530

    init

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/chetanguptaa/internal/handlers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func healthcheck(c *fiber.Ctx) error {
+	return c.SendString("OK")
+}
+func main() {
+	app := fiber.New()
+	app.Use("/api", func(c *fiber.Ctx) error {
+		fmt.Println("Hello from Middleware")
+		return c.Next()
+	})
+	app.Get("/healthcheck", healthcheck)
+	app.Post("/api/products", handlers.CreateProduct)
+	app.Get("/api/products", handlers.GetAllProducts)
+	fmt.Println("Hello World!")
+	log.Fatal(app.Listen(":3000"))
+}
